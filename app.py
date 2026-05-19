7b11a491305b83c6ebbb9636c7c6026b6c006573
Johny Feitosa <joniii5@hotmail.com>
2026-05-18 21:59:34 -0300

    git commit -m

--- app.py
+++ app.py
@@ -20,6 +20,10 @@ def sobre():
     return '<h1>Sobre a Empresa</h1>'
 
 # Quarta rota usando Flask Página de vendas GET, POST, PUT, DELETE
+@app.route('/vendas', methods=['GET'])
+def Pagina_de_vendas():
+    return '<h1>Notebook Dell</h1>'
+       
 
 
 # roda o projeto
