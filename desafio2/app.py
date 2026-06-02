45193a22d5838331bce9bebbf3abc2113cb93d18
Johny Feitosa <joniii5@hotmail.com>
2026-06-02 10:29:46 -0300

    commnt added

--- desafio2/app.py
+++ desafio2/app.py
@@ -6,14 +6,15 @@ app = Flask(__name__)
 # GET e POST
 # GET: coletar os dados
 #POST: insere dados
-@app.route('/', methods=['GET e POST'])  # @app.route('/'): Define que este código deve ser executado quando alguém acessar a página inicial do seu site (a raiz, representada pela barra /).
-                                         # methods=['GET', 'POST']: Define quais tipos de "conversas" o servidor aceita nesse endereço.
+@app.route('/', methods=['GET e POST'])  
+# @app.route('/'): Define que este código deve ser executado quando alguém acessar a página inicial do seu site (a raiz, representada pela barra /).
+# methods=['GET', 'POST']: Define quais tipos de "conversas" o servidor aceita nesse endereço.
 #def é função no python
 def cadastro():
     if request.method == 'POST':
        nome      = request.form.get('nome')
        telefone  = request.form.get('telefone')
-    return render_template('cadastro.html')
+    return render_template('cadastro.html') # exibe um arquivo html em resposta a uma requisição do usuário
 
 # isso faz o projeto rodar
 if __name__ == '__Main__':
